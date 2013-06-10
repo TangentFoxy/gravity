cc2df641d6f2c9b830da407ca7e5b6abdd69f899
Paul Liverman <paul.liverman.iii@gmail.com>
2013-06-09 21:43:15 -0700

    made setOrbitalVelocity work with negative mass
    Of course, you can't orbit something with negative mass, so you'll still
    get flung into space when doing this, but at least it doesn't just fail.

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ function setOrbitalVelocity(id,d)
 {
     //id=parent body id, d=radius of orbit (distance)
     //change to set this value relative to the parent body, applying any accelerations to the parent to the child
-    var result=Math.sqrt(G*objects[id].m/(d*1.25));
+    var result=Math.sqrt(Math.abs(G*objects[id].m/(d*1.25)));
     consoleOut("Calculated orbital velocity: "+result,"Calculated orbital velocity: "+result);
     return result;
 }
