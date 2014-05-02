226f517864f1d507f6ec592defe10e1f3ffe60fc
Paul Liverman <paul.liverman.iii@gmail.com>
2014-05-02 14:37:14 -0700

    removed unneeded code / comments

--- grav.refactor/custom/main.js
+++ grav.refactor/custom/main.js
@@ -3,38 +3,21 @@ var focusBody=0;
 
 function initialize() {
 	bodies=new randomSystem(2,20);
-	barycenter=physics.calculateBarycenterVector(bodies);
 	game=new Interval('loop();',timing);
-	// this is supposed to work with barycenters (and it does initially, but breaks quickly)
-	//render[1].setTransform(1,0,0,1,barycenter.x+window.innerWidth/2,barycenter.y+window.innerHeight/2);
 }
 
 function loop(){
 	forEachCompare(bodies,function(a,b){
 		physics.applyGravity(a,b);
-		//
+		//do orbital stuff
 	});
 	forEach(bodies,function(b){physics.updateLocation(b);});
-	// barycenters tend to not stay correct for long,
-	// this may be because of glitchiness with colliding things that don't collide
-	//physics.updateLocation(barycenter);
-	barycenter=physics.calculateBarycenterVector(bodies);
 
 	redrawIdontLike();
-	/* these should work for Barycenter based drawing, but don't
-	render[1].setTransform(1,0,0,1,barycenter.x+window.innerWidth/2,barycenter.y+window.innerHeight/2);
-	render[1].clearRect(barycenter.x-window.innerWidth/2,barycenter.y-window.innerHeight/2,window.innerWidth,window.innerHeight);
-	*/
-	// this line was used to draw a Body WHERE IT WAS
-	//forEach(bodies,function(b){b.fill();});
-	// this line was when using static position
-	//render[1].clearRect(-window.innerWidth/2,-window.innerHeight/2,window.innerWidth,window.innerHeight);
 }
 
 function randomSystem(min,max){
-	//2*radius of star*2.8 should be where habitable zone is?
-	//maybe even closer, like 2*radius*2.5 TO 2*radius*3.2 ???
-	// 2*radius*2.4 TO 2*radius*4.2 seems best!?
+	// Habitable zone will be based on star: 2*radius*2.4 TO 2*radius*4.2
 	var total=random.integer(min,max);
 	var b=[];
 	b[0]=new Body(random.number(50,100),0,0);
@@ -42,7 +25,6 @@ function randomSystem(min,max){
 		b[i]=new Body(random.number(5,10),
 			random.number(-window.innerWidth/2,window.innerWidth/2),
 			random.number(-window.innerHeight/2,window.innerHeight/2));
-		//this is where a setOrbit call would go
 		physics.setOrbit(b[0],b[i]);
 	}
 	console.log("Generated "+b.length+" bodies.");
@@ -67,16 +49,3 @@ function Body(radius,x,y,color,rotationSpeed){
 
 var version="0.1.2r";	//version number of your program
 document.title='Gravity v'+version+' Jenjens v'+engineVersion;
-/*
-terminal.clearLast=true;
-terminal.background=null;
-terminal.color='black';
-terminal.font='12px sans-serif';
-terminal.textHeight=12;
-terminal.xOffset=4;
-terminal.yOffset=4;
-terminal.logging=true;
-
-var debugColor='maroon';	//used in debug tools drawing things
-var debugLineWidth=1.2;		//used in debug tools drawing things
-*/
--- grav.refactor/custom/physics.js
+++ grav.refactor/custom/physics.js
@@ -2,15 +2,12 @@ physics.G=1/5;			//Gravitational Constant (incorrect by default)
 physics.timeStep=0.5;	//timeStep is multiplied by physics calculations
 						// to adjust speed vs accuracy of simulation
 
-//alternate gravity thing which is weird:
-// this apparently works AND checks for collisions?
-//  and requires a lot less calculation
-//   test this
 function altGravity(a,b){
+	//wtf is this
 	var rx=a.x-b.x;		var ry=a.y-b.y;
 	var R2=rx*rx+ry*ry;	var Rdiv=Math.pow(R2,-1.5);
-	a.Vx-=a.mass*Rdiv*rx;	b.Vx+=b.mass*Rdiv*rx;
-	a.Vy-=a.mass*Rdiv*ry;	b.Vy+=b.mass*Rdiv*ry;
+	a.Vx-=a.mass*Rdiv*rx*physics.timeStep;	b.Vx+=b.mass*Rdiv*rx*physics.timeStep;
+	a.Vy-=a.mass*Rdiv*ry*physics.timeStep;	b.Vy+=b.mass*Rdiv*ry*physics.timeStep;
 }
 
 function getParentAndInfluencer(a,b){
