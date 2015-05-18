411ff092015bd0950bcdb84db8105d6bc88a3cf1
Paul Liverman <paul.liverman.iii@gmail.com>
2015-05-17 23:53:11 -0700

    screwing with gource

---
+++ gource-video.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+REM this only works if you open command prompt yourself and navigate here because fuck consistency?
+"C:\Program Files (x86)\Gource\gource.exe" -1280x720 -user-image-dir user-images -b 223355 -title Gravity
